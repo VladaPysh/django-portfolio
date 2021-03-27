6ca78e7e4eded037228b7e64639668ec165b14f5
vladapysh <vlada.pysh1@gmail.com>
2021-03-27 21:19:44 +0200

    set backend for send emal function

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,4 +126,9 @@ MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'portfolio/static/portfolio/images')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'vlada.pysh1@gmail.com'
+EMAIL_HOST_PASSWORD = '******'
